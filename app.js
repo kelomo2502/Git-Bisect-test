eab287ba1b3e4a82cd8fc423b69a5efc7291f678
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-06-27 11:22:40 +0100

    Add background

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const sportsContainer = document.querySelector('.sports');
 for (let i = 0; i < sportsList.length; i++) {
   const sportsLi = document.createElement('li');
   sportsLi.innerText = sportsList[i];
+  sportsLi.setAttribute('class', 'sports-li')
   sportsContainer.appendChild(sportsLi);
 }
+
+sportsContainer.addEventListener('click', function (e) {
+  console.log(e.target.getAttribute('class') + ' is clicked');
+  const target = e.target;
+  target.style.backgroundColor = 'lightgrey';
+})
 
